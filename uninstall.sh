134eac1a7adb01ab87e9880014027578a2f008be
DevHerles <=>
2019-05-30 12:49:32 -0500

    Fix uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 function uninstall() {
-    sudo rm .zshrc
     sudo rm -rf .dotfiles
-    sudo rm .tmux.conf   
-    sudo rm -rf .tmux    
-    sudo rm -rf .oh-my-zsh 
+    sudo rm -rf .tmux
+    sudo rm -rf .oh-my-zsh
     sudo rm -rf .powerlevel10k
+    sudo rm .zshrc
+    sudo rm .vimrc
+    sudo rm .tmux.conf
 }
 
 uninstall
