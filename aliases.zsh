0a23cab3938a88090dfaa08b1595e43aa4b2d5f1
DevHerles <=>
2019-07-18 09:54:45 -0500

    Improve git push -f alias

--- aliases.zsh
+++ aliases.zsh
@@ -19,3 +19,6 @@ alias cls='clear'
 
 alias glop="git log --graph --pretty=format:'%C(#d79921)%h%Creset -%C(yellow)%d%Creset %C(#83a598)%s%Creset %C(#fe8019)(%cr) %C(#665c54)<%an>%Creset' --abbrev-commit --date=relative"
 alias glo="git log --oneline --pretty=format:'%C(#d79921)%h%Creset -%C(yellow)%d%Creset %C(#83a598)%s%Creset %C(#fe8019)(%cr) %C(#665c54)<%an>%Creset' --abbrev-commit --date=relative"
+alias gpf="git add . & git commit --amend --no-edit & git push -f"
+alias gca="git add . & git commit --amend --no-edit"
+alias gdf="git diff"
