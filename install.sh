84ac8e1cfbd49f3204b56f26777b8c82b4ecf0ee
HerlesINC <herles.incalla@gmail.com>
2020-05-07 14:11:19 -0500

    FIX install latest NVIM

--- install.sh
+++ install.sh
@@ -139,7 +139,11 @@ function setupVim() {
     echo "Neovim is already installed"
   else
     echo "Installing Neovim..."
+    sudo apt-get install software-properties-common
+    sudo add-apt-repository ppa:neovim-ppa/stable
+    sudo apt-get update
     sudo apt-get install neovim
+    sudo apt-get install python-dev python-pip python3-dev python3-pip
     sudo apt-get install python-neovim
     sudo apt-get install python3-neovim
   fi
